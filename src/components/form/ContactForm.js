b00640dee0a1715643f0f61a7421b3ff9b854ba3
Diego Barrales <diego.barrales@csu.fullerton.edu>
2024-05-15 18:37:12 -0700

    style contact form

--- src/components/form/ContactForm.js
+++ src/components/form/ContactForm.js
@@ -1,9 +1,77 @@
+import { Box, Button, TextField, InputAdornment } from "@mui/material";
+import Email from "@mui/icons-material/Email";
+import PersonIcon from "@mui/icons-material/Person";
+
 const ContactForm = () => {
+  const Form = () => {
+    return (
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "50ch" },
+        }}
+        noValidate
+        autoComplete="off"
+        display="grid"
+        justifyContent="center"
+        justifyItems="center"
+      >
+        <TextField
+          id="name"
+          label="Name"
+          variant="outlined"
+          required
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <PersonIcon />
+              </InputAdornment>
+            ),
+          }} />
+
+        <TextField
+          id="email"
+          label="Email"
+          variant="outlined"
+          required
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Email />
+              </InputAdornment>
+            ),
+          }} />
+
+        <TextField
+          id="message"
+          label="Message"
+          variant="outlined"
+          required
+          multiline
+          fullWidth
+          rows={4} />
+
+        <Button
+          variant="contained"
+          // TODO: connect with either automatic system or create backend for email submissions 
+          sx={{ borderRadius: "100vw" }}
+        >
+          Send
+        </Button>
+      </Box>
+    );
+  }
+
   return (
-    <form>
-      <label>Name:</label>
-      <input type="text" />
-    </form>
+    // <Grid container>
+    //   <Grid item>
+    // TODO: create image for here
+    //   </Grid>
+    //   <Grid item>
+    <Form />
+    // </Grid>
+
+    // </Grid>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,17 +11,16 @@ const Home = () => {
       <Container maxWidth="lg" sx={{ textAlign: "center", marginBottom: 4 }}>
         <main>
           <section id="AboutMe">
-            <Typography variant="h2" my={3}>About me!</Typography>
+            <Typography variant="h2" my={3}>About Me</Typography>
             <Typography my={1} textAlign="justify" className="paragraph">
-              Stationed in Los Angeles, California
-              Graduating from California State University, Fullerton in 2024 as a Computer Science major
-              Loves coffee, drawing, and anime.
+              I live in Los Angeles, California and am graduating from California State University, Fullerton in 2024 as a Computer Science major.
+              I Love coffee, drawing, and anime.
               I consider my family to be a big part of who I am because they are one of the reasons I am here at Fullerton, studying computer science; They push me to be the best I can be.
               {/* TODO: add more in about me */}
             </Typography>
           </section>
           <section id="ContactMe">
-            <Typography variant="h2" my={3}>Connect With Me!</Typography>
+            <Typography variant="h2" my={3}>Send Me A Message</Typography>
             <ContactForm />
           </section>
         </main>
