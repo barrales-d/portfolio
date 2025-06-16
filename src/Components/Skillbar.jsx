16dc20df1b792b3b9396d4d39689cdee1c60a1b5
Diego Barrales <dbarrales06@gmail.com>
2025-06-15 20:15:49 -0700

    Move Skill data to JSON file + Added Filling up animation

--- src/Components/Skillbar.jsx
+++ src/Components/Skillbar.jsx
@@ -1,17 +1,32 @@
 import { Box, Typography, LinearProgress, linearProgressClasses } from "@mui/material";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 
 const Skillbar = ({ data }) => {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    if(progress < data.percentage) {
+      const interval = setInterval(() => {
+        setProgress(prev => {
+          const next = prev + 1;
+          return next >= data.percentage ? data.percentage : next;
+        })
+      }, 10);
+      return () => clearInterval(interval);
+    }
+
+  }, [data.percentage, progress]);
+
   return (
     <Fragment>
       <Box display="flex" justifyContent="space-between" alignItems="baseline">
-        <Typography variant="h6">{data.Title}</Typography>
-        <Typography variant="body2">{data.Percentage}&#37;</Typography>
+        <Typography variant="h6">{data.name}</Typography>
+        <Typography variant="body2">{data.percentage}&#37;</Typography>
       </Box>
       <Box width="100%">
         <LinearProgress
           variant="determinate"
-          value={data.Percentage}
+          value={progress}
           sx={{
             height: '1rem',
             borderRadius: 5,
--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -1,51 +1,17 @@
 import { Grid } from "@mui/material";
-import { useMemo } from "react";
 
 import Skillbar from "../Components/Skillbar";
 import Wrapper from "../Components/Wrapper";
 
+import skills from "../Data/skills.json";
+
 const Skills = ({ animation }) => {
-  const skills = useMemo(() => {
-    let iota = 0;
-    function updateIota() {
-      const current = iota;
-      iota += 1;
-      return current;
-    }
-    return [
-      {
-        ID: updateIota(),
-        Title: 'C++',
-        Percentage: 85
-      },
-      {
-        ID: updateIota(),
-        Title: 'Javascript',
-        Percentage: 83
-      },
-      {
-        ID: updateIota(),
-        Title: 'HTML / CSS',
-        Percentage: 94
-      },
-      {
-        ID: updateIota(),
-        Title: 'Python',
-        Percentage: 68
-      },
-      {
-        ID: updateIota(),
-        Title: 'Problem Solving',
-        Percentage: 92
-      }
-    ]
-  }, []);
   return (
     <Wrapper title='Skills' animation={animation}>
       <Grid container mb={4} spacing={6}>
         {
           skills.map(skill => (
-            <Grid item key={skill.Title + '-' + skill.ID} xs={12} md={6}>
+            <Grid item key={skill.name} xs={12} md={6}>
               <Skillbar data={skill}> </Skillbar>
             </Grid>
           ))
